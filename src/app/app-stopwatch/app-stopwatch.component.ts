e93bfa1f33eb364ddb5f2a1170a2e963e7bd512e
Martin Peeters <406336@student.fontys.nl>
2020-09-14 11:13:37 +0200

    basic starting mechanism of stopwatch

--- src/app/app-stopwatch/app-stopwatch.component.ts
+++ src/app/app-stopwatch/app-stopwatch.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Time} from '@angular/common';
+import {StopwatchService} from '../services/stopwatch.service';
+import {catchError, first, map} from 'rxjs/operators';
+import {throwError} from 'rxjs';
 
 @Component({
   selector: 'app-app-stopwatch',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppStopwatchComponent implements OnInit {
 
-  constructor() { }
+  // Variables
+  stopwatchTime: Time;
+
+  // Constructor & onInit
+  constructor(private stopwatchService: StopwatchService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  // Methods
+  startStopwatch(): void {
+    console.log('Start!');
+    this.stopwatchService.startStopwatch()
+
+      // Map and edit the data so it fits the front end
+      .pipe(map(data => {
+        console.log(data);
+        return data.toLocaleString() + 's';
+      }),
+        // Catch errors if needed
+        catchError(err => {
+        return throwError(err);
+      }))
+
+
+      // Start the process and display data
+      .subscribe(subData => {
+        console.log(subData);
+        this.stopwatchTime = subData;
+        },
+        error => {
+          console.log('Error!');
+        },
+        () => {
+          console.log('Completed!');
+        });
+
   }
 
 }
